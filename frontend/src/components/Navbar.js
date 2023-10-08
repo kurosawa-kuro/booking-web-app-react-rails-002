32f4d401bf2f10c4bbbbfa8cf324068604b9e893
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-10-08 07:45:37 +0000

    add

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useAuthStore } from "../state/store";
 
 function Navbar() {
-    const { userInfo, logout } = useAuthStore();
+    const { logout } = useAuthStore();
 
     function handleLogout(e) {
         e.preventDefault();
@@ -10,7 +10,7 @@ function Navbar() {
         window.location.href = '/login';
     }
 
-    console.log("userInfo", userInfo);
+    const isAdmin = localStorage.getItem('userInfo') && JSON.parse(localStorage.getItem('userInfo')).admin;
 
     return (
         <div className="bg-gray-800 text-white py-4">
@@ -20,20 +20,22 @@ function Navbar() {
                     <div className="space-x-4">
                         {localStorage.getItem('userInfo') ? (
                             <>
-
-                                <a href="/admin/users" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">ユーザー一覧</a>
-                                <a href="/admin/bookings" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">予約一覧</a>
-                                <a href="/admin/rooms" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">部屋一覧</a>
-                                <a href="/admin/rooms/new" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">部屋新規作成</a>
-                                <a href="/admin/tags" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">タグ一覧</a>
-                                <a href="/admin/tags/new" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">タグ新規作成</a>
-                                <a href="/bookings" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">予約一覧</a>
-
+                                {isAdmin ? (
+                                    <>
+                                        <a href="/admin/users" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">ユーザー一覧</a>
+                                        <a href="/admin/bookings" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">予約一覧</a>
+                                        <a href="/admin/rooms" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">部屋一覧</a>
+                                        <a href="/admin/rooms/new" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">部屋新規作成</a>
+                                        <a href="/admin/tags" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">タグ一覧</a>
+                                        <a href="/admin/tags/new" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">タグ新規作成</a>
+                                    </>
+                                ) : (
+                                    <a href="/bookings" className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">予約一覧</a>
+                                )}
                                 <span className="bg-yellow-500 px-3 py-1 rounded-lg shadow-md">
                                     {JSON.parse(localStorage.getItem('userInfo')).email}
                                 </span>
-                                <a href="/logout" onClick={handleLogout} className="transition-colors duration-200 px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:outline-none">ログアウト</a>
-                                {/* <a href="/logout" onClick={handleLogout} className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100">ログアウト</a> */}
+                                <a href="/logout" onClick={handleLogout} className="transition-colors duration-200 px-4 py-2 rounded bg-red-500 hover:bg-red-400 focus:bg-red-600 focus:outline-none">ログアウト</a>
                             </>
                         ) : (
                             <>
